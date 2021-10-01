0a92e2ab8c1881282236cca4e23a39819442821d
Matthew <matthew.taylor@coveros.com>
2021-10-01 16:36:08 -0400

    Groovy syntax

--- vars/smokeTest.groovy
+++ vars/smokeTest.groovy
@@ -1,7 +1,7 @@
 def call(String appUrl="") {
     echo 'Perform smoke test for system viability'
     healthCheck = sh(script: "curl ${appUrl}/health-check", returnStatus: true)
-    if healthCheck != 0 {
+    if (healthCheck != 0) {
         echo "Error found running the smoke test."
         currentBuild.result = 'FAILURE'
         throw err
